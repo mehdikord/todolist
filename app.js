ef2c0db8de5d154b6c70272c274145077eada17b
Mehdi Kord <mehdi.kord171@gmail.com>
2024-06-09 10:22:00 +0330

    updated

--- app.js
+++ app.js
@@ -2,11 +2,27 @@ const { createApp } = Vue
 
 createApp({
 
+  beforeCreate(){
+    console.log("this is before created")
+
+  },
+
   created(){
+    console.log("page is created")
 
     this.GetGroup();
 
   },
+
+  mounted(){
+    console.log("page is mounted")
+
+  },
+  updated(){
+    console.log("page is updated")
+
+  },
+
 
   data() {
     return {
@@ -15,6 +31,10 @@ createApp({
             color : null,
             description : null
           },
+      add_item : {
+            id : null,
+            item:null,
+      },
       group : []
 
     }
@@ -32,6 +52,7 @@ createApp({
         this.add.color = "#212121";
       }
       this.group.push({
+        id : Math.floor(Math.random() * 1000000),
         name : this.add.name,
         color : this.add.color,
         description : this.add.description
@@ -43,6 +64,7 @@ createApp({
       this.add.description = null
 
     },
+
     GetGroup(){
       if (localStorage.getItem('todolist_group')){
 
@@ -50,6 +72,29 @@ createApp({
 
 
       }
+    },
+
+    AddItem(){
+
+      if (!this.add_item.item || !this.add_item.id){
+        return alert('اطلاعات ناقض است')
+      }
+
+      this.group.filter(item => {
+        if (item.id === this.add_item.id){
+          if (!item.list){
+            item.list =[];
+          }
+          item.list.push({
+            id : Math.floor(Math.random() * 1000000),
+            item : this.add_item.item
+          })
+        }
+      })
+      localStorage.setItem('todolist_group',JSON.stringify(this.group));
+      this.add_item.item=null;
+
+
     }
 
   }
